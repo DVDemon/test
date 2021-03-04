587bcf49dd533d98253a2cfe3594ab88d24dcfd5
Dmitriy Dzyuba <ddzuba@yandex.ru>
2021-03-04 17:08:50 +0000

    fix google test

--- tests.cpp
+++ tests.cpp
@@ -1,16 +1,18 @@
 #include <gtest/gtest.h>
 #include "mylib.h"
 
-TEST(test_add_figure, basic_test_set){
-testing::internal::CaptureStdout();
+TEST(test_add_figure, basic_test_set)
+{
+    testing::internal::CaptureStdout();
 
-foo();
+    foo();
 
-std::string output = testing::internal::GetCapturedStdout();
-ASSERT_TRUE(output=="Hello from lib");
+    std::string output = testing::internal::GetCapturedStdout();
+    ASSERT_TRUE(output == "Hello from lib");
 }
 
-int main(int argc, char **argv) {
-testing::InitGoogleTest(&argc, argv);
-return RUN_ALL_TESTS();
+int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
